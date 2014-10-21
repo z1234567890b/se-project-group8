7bc4838e2c989598be0589312354742288c48615
z1234567890b@gmail.com <z1234567890b@gmail.com@5f2dbf48-14dc-350a-a83d-0fddc383f475>
2014-10-21 15:09:34 +0000

    test3

--- SMS-Messenger-Group8/src/project/se3354/sms_messenger_group8/MainActivity.java
+++ SMS-Messenger-Group8/src/project/se3354/sms_messenger_group8/MainActivity.java
@@ -1,18 +1,27 @@
-//Test 2, modification
 package project.se3354.sms_messenger_group8;
 
 import android.support.v7.app.ActionBarActivity;
+import android.content.DialogInterface;
+import android.content.DialogInterface.OnClickListener;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends ActionBarActivity implements OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
+        
+        TextView sms_tv = (TextView) findViewById(R.id.sms_tv);
+        Button send_btn = (Button) findViewById(R.id.send_btn);
+        send_btn.setOnClickListener(MainActivity.this);
     }
 
 
@@ -34,4 +43,18 @@ public class MainActivity extends ActionBarActivity {
         }
         return super.onOptionsItemSelected(item);
     }
+
+
+	@Override
+	public void onClick(DialogInterface dialog, int which) {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+	@Override
+	public void onClick(View v) {
+		// TODO Auto-generated method stub
+		
+	}
 }
