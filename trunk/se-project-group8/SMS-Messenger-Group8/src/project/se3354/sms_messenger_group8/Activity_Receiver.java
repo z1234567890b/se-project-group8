337050797879a059c173c3a4382654fe1251fe4c
z1234567890b@gmail.com <z1234567890b@gmail.com@5f2dbf48-14dc-350a-a83d-0fddc383f475>
2014-11-12 21:48:46 +0000

    Auto Reply with fixed sentence. It will be improved.

--- trunk/se-project-group8/SMS-Messenger-Group8/src/project/se3354/sms_messenger_group8/Activity_Receiver.java
+++ trunk/se-project-group8/SMS-Messenger-Group8/src/project/se3354/sms_messenger_group8/Activity_Receiver.java
@@ -1,15 +1,22 @@
 package project.se3354.sms_messenger_group8;
 
+import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.telephony.SmsManager;
 import android.telephony.SmsMessage;
 import android.widget.Toast;
+import android.widget.CompoundButton;
+import android.widget.ToggleButton;
 
 /* device receive the message and give a popup with the number and message body*/
 public class Activity_Receiver extends BroadcastReceiver
-{
+{        
+	private static String PhoneNoRec = "";
+    private static String MessageRec = "";
+    //private constant 
 	 
     @Override
     public void onReceive(Context context, Intent intent) 
@@ -17,22 +24,47 @@ public class Activity_Receiver extends BroadcastReceiver
         /*get the message passed in */
         Bundle bundle = intent.getExtras();        
         SmsMessage[] msgs = null;
-        String smsString = "";            
+        String smsString = "";  
+        String oldSMS = "";
+        int j=0;
+        
+        
         if (bundle != null)
         {
             /*retrieve the message received*/
             Object[] pdus = (Object[]) bundle.get("pdus");
-            msgs = new SmsMessage[pdus.length];            
+            msgs = new SmsMessage[pdus.length]; 
+
+            
             for (int i=0; i<msgs.length; i++){
-            	msgs[i] = SmsMessage.createFromPdu((byte[])pdus[i]);                
-                smsString += "SMS from " + msgs[i].getOriginatingAddress()+" : "+"\n";                     
-                smsString +=msgs[i].getMessageBody().toString()+"\n";     
+            	msgs[i] = SmsMessage.createFromPdu((byte[])pdus[i]);
+            	PhoneNoRec=msgs[i].getOriginatingAddress();
+            	MessageRec=msgs[i].getMessageBody().toString();
+                smsString += "SMS from " + PhoneNoRec+" : "+"\n"+MessageRec+"\n";                   
             }
+            
             /* display the new message */
-            Toast.makeText(context, "SMS Received", Toast.LENGTH_LONG).show();
+            Toast.makeText(context, "SMS Received", Toast.LENGTH_SHORT).show();
             /*Write newly received message in the TextView box*/
-            MainActivity addRev=new MainActivity();
-            addRev.txtReceive.setText(smsString);
+            MainActivity activity1st=new MainActivity();
+            activity1st.txtReceive.setText(smsString);
+            
+            //Autoreply: if Auto_Reply button is on, reply "I am not available"
+            if (activity1st.autoReplyOn==1){
+            	// if two devices have "Auto_Reply" on, they will reply each other 2 times
+            	if (j<2){
+            		if (PhoneNoRec.equals(oldSMS)){
+            			j+=1;
+            		}else{
+            			j=0;
+            		}
+	            	SmsManager sms = SmsManager.getDefault();
+	            	//String ar = activity1st.txtAutoReply.getText().toString();
+	            	sms.sendTextMessage(PhoneNoRec, null, "I am not available", null, null);
+	            	oldSMS=PhoneNoRec;
+	            	
+            	}
+            }
         } 
         
         /* tell the inbox it needs to update */
