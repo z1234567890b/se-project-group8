8d4dc303347096e52ddb3fd329d76f3ae765713e
karl.taht@gmail.com <karl.taht@gmail.com@5f2dbf48-14dc-350a-a83d-0fddc383f475>
2014-11-12 09:34:39 +0000

    Added Conversation Activity. (No data yet, just a test of version control)

---
+++ se-project-group8/SMS-Messenger-Group8/src/project/se3354/sms_messenger_group8/Activity_Conversation.java
@@ -0,0 +1,16 @@
+package project.se3354.sms_messenger_group8;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Activity_Conversation extends Activity {
+
+	/** Called when the activity is first created. */
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+	    super.onCreate(savedInstanceState);
+	
+	    // TODO Auto-generated method stub
+	}
+
+}
