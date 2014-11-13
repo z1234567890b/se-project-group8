17c1bcb51f689f1b730ae65d03eca5ac28083235
2012DCheng@gmail.com <2012DCheng@gmail.com@5f2dbf48-14dc-350a-a83d-0fddc383f475>
2014-11-13 04:04:50 +0000

    Contact Name is now displayed instead of phone number (In both inbox and conversation). However, loading this data is slow. Need to find out how to implement loaders in order for app to meet desirable levels of robustness (At least 5 second delay on loading inbox).

--- trunk/se-project-group8/SMS-Messenger-Group8/src/project/se3354/sms_messenger_group8/MyMessage.java
+++ trunk/se-project-group8/SMS-Messenger-Group8/src/project/se3354/sms_messenger_group8/MyMessage.java
@@ -5,6 +5,7 @@ import android.graphics.Bitmap;
 public class MyMessage {
 	//private Bitmap icon;
     private String contactName = null;
+    private String phoneNumber = null;
     private String messageDate = null;
     private String messageBody = null;
     private String messageType = null;
@@ -12,6 +13,7 @@ public class MyMessage {
     
     //public Bitmap getIcon() {return icon;}
     public String getContactName() {return contactName;}
+    public String getPhoneNumber() {return phoneNumber;}
     public String getMessageDate() {return messageDate;}
     public String getMessageBody() {return messageBody;}
     public String getMessageType() {return messageType;}
@@ -19,6 +21,7 @@ public class MyMessage {
     
     //public void setIcon(Bitmap icon) {this.icon = icon;}
     public void setContactName(String contactName) {this.contactName = contactName;}
+    public void setPhoneNumber(String phoneNumber) {this.phoneNumber = phoneNumber;}
     public void setMessageDate(String messageDate) {this.messageDate = messageDate;}
     public void setMessageBody(String messageBody) {this.messageBody = messageBody;}
     public void setMessageType(String messageType) {this.messageType = messageType;}
