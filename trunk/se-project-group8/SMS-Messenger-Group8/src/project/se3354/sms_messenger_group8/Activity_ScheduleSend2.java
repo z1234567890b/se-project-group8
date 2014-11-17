71a7cc93da629e322eac18ff9f8344407606acfe
z1234567890b@gmail.com <z1234567890b@gmail.com@5f2dbf48-14dc-350a-a83d-0fddc383f475>
2014-11-17 21:36:58 +0000

    Version 2 of Scheduled Sending. Still need to update the sending method to save into local database. Introduction: 1. Gave number and text (required), click "SendLater", get to new layout to add delayed time (>1 second), click schedule to set timer alarm. Time will be shown at bottom. 2. You can go back to do other thing during waiting time, or you can cancel it. If go back, you will find the SendLater button changed to be red "Scheduled". It will be red during waiting. 3. you can click red "Scheduled" button to get in again. You can see scheduled time, and you can choose back or cancel 4. I tried to call smsSend method in MainActivity, but failed. So, still need to update the sending method.

---
+++ trunk/se-project-group8/SMS-Messenger-Group8/src/project/se3354/sms_messenger_group8/Activity_ScheduleSend2.java
@@ -0,0 +1,86 @@
+package project.se3354.sms_messenger_group8;
+
+import android.app.Activity;
+import android.app.AlarmManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class Activity_ScheduleSend2 extends Activity {
+	
+	static TextView textView2;
+	Button btnBack;
+	Button btnCancel;
+	static TextView txtNumber;
+	static TextView txtText;
+	static TextView txtTime;
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_schedulesend2);
+				
+		textView2 = (TextView) findViewById(R.id.textView2);
+		btnBack = (Button) findViewById(R.id.button2);
+		btnCancel = (Button) findViewById(R.id.button3);
+		txtNumber = (TextView) findViewById(R.id.txtNumber);
+		txtText = (TextView) findViewById(R.id.txtText);
+		txtTime = (TextView) findViewById(R.id.txtTime);
+		
+		//Read number and text to Scheduled2 page
+		txtNumber.setText(Activity_ScheduleSend.txtNumber.getText().toString());
+		txtText.setText(Activity_ScheduleSend.txtText.getText().toString());
+		//txtTime.setText (Activity_ScheduleSend.txtTime.getText().toString());
+		textView2.setText (Activity_ScheduleSend.txtTime.getText().toString());
+		
+
+	btnBack.setOnClickListener(new View.OnClickListener() {
+    	public void onClick(View v) {
+    		//Go back to previous page, keep old information
+    		finish();
+    	}
+    });
+
+}
+	
+
+public void cancelAlarm(View v) {
+		// create the AlarmManager object
+		AlarmManager alarmManager = (AlarmManager) getSystemService(Context.ALARM_SERVICE);
+		// create an Intent and set the class which will execute when Alarm triggers
+	    Intent intentAlarm = new Intent(this, Activity_AlarmReciever.class);
+		alarmManager.cancel(PendingIntent.getBroadcast(this,1,  intentAlarm, PendingIntent.FLAG_UPDATE_CURRENT));
+		//Go back to clean main page
+		Intent myIntent = new Intent(v.getContext(), MainActivity.class);
+	    startActivityForResult(myIntent, 0);
+	}
+
+
+
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.activity__schedule_send2, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// Handle action bar item clicks here. The action bar will
+		// automatically handle clicks on the Home/Up button, so long
+		// as you specify a parent activity in AndroidManifest.xml.
+		int id = item.getItemId();
+		if (id == R.id.action_settings) {
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
+	}
+}
