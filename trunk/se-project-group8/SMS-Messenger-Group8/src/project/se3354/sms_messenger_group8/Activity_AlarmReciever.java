f9d3da6c6fb9c966edcd65dfec11177c6a7096c2
z1234567890b@gmail.com <z1234567890b@gmail.com@5f2dbf48-14dc-350a-a83d-0fddc383f475>
2014-11-18 05:34:47 +0000

    Version3 of Scheduled Sending.  Message will be displayed in bottom box in main page after sending.

--- trunk/se-project-group8/SMS-Messenger-Group8/src/project/se3354/sms_messenger_group8/Activity_AlarmReciever.java
+++ trunk/se-project-group8/SMS-Messenger-Group8/src/project/se3354/sms_messenger_group8/Activity_AlarmReciever.java
@@ -1,34 +1,46 @@
 package project.se3354.sms_messenger_group8;
 
+import java.util.Date;
+import android.content.pm.PackageManager;
 import android.content.BroadcastReceiver;
+import android.content.ComponentName;
+import android.content.ContentValues;
 import android.content.Context;
 import android.content.Intent;
+import android.database.Cursor;
+import android.net.Uri;
 import android.telephony.SmsManager;
 import android.widget.Toast;
 import android.app.Activity;
+import android.app.PendingIntent;
+import android.app.ListActivity;
+import android.app.PendingIntent;
+import android.content.ContentValues;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.net.Uri;
+import android.os.Bundle;
+import android.provider.Telephony;
+
+import java.lang.Object;
+
 
 public class Activity_AlarmReciever extends BroadcastReceiver
 {
+	Activity activity = new Activity();
          @Override
             public void onReceive(Context context, Intent intent)
             {
-                    
-                
-                    
-                      // here you can start an activity or service depending on your need
-                     // for ex you can start an activity to vibrate phone or to ring the phone   
-        	 		//Activity_ScheduleSend sendLater = new Activity_ScheduleSend();                 
                     String phoneNumberReciver=Activity_ScheduleSend.txtNumber.getText().toString();
                     String message=Activity_ScheduleSend.txtText.getText().toString();
-                    SmsManager sms = SmsManager.getDefault(); 
-                    sms.sendTextMessage(phoneNumberReciver, null, message, null, null);
                     
-                    /*---Doesn't work
-                    MainActivity laterSend = new MainActivity();
-                    laterSend.sendSMS("5556", "message");
-                    */
                     
+                    sendSchedule (phoneNumberReciver, message);
+                    
+                                                           
                     Toast.makeText(context, "Scheduled SMS has been Sent", Toast.LENGTH_SHORT).show();
+                    
                     //Clear SendLater information
                     Activity_ScheduleSend.txtNumber.setText("");
                     Activity_ScheduleSend.txtText.setText("");
@@ -44,10 +56,67 @@ public class Activity_AlarmReciever extends BroadcastReceiver
                 	Activity_ScheduleSend2.textView2.setText("Scheduled SMS has been sent!!! \n Please go back.");
                 	
                 	
-                	
-                    
-                   
+ 
              }
-         
+   
+         Context context;
+         public void sendSchedule(String phoneNumber, String message)
+         {      	
+        	 
+                 try {
+                     SmsManager sms = SmsManager.getDefault();           
+                     sms.sendTextMessage(phoneNumber, null, message, null, null);
+
+                     // manage "content://sms" since we are the default sms app
+                     //saveUsersentSMS(phoneNumber, message);
+                     
+                   //Add time and date at the end
+                     Date resultdate = new Date(System.currentTimeMillis());
+                     MainActivity.txtReceive.setText("SMS sent to "+phoneNumber + ": "+"\n"+"["+ message+"]\n"+ resultdate);
+                 } 
+                 // Not sure how to test exception
+                 catch (Exception e) {
+                	 
+                     Toast.makeText(context,"SMS failed", Toast.LENGTH_LONG).show();
+                     Date resultdate = new Date(System.currentTimeMillis());
+                     MainActivity.txtReceive.setText("SMS sent to "+"<"+phoneNumber+"> :"+"\n"
+                     		+"["+ message+"]\n" +" !!!SMS failed\n"+resultdate);
+                     e.printStackTrace();
+                 }
+             
+             
+         }
+     	
+     	public void saveUsersentSMS(String recipient, String body) {
+             Uri threadIdUri = Uri.parse("content://mms-sms/threadID");
+             Uri.Builder builder = threadIdUri.buildUpon();
+             builder.appendQueryParameter("recipient", recipient);
+             Uri uri = builder.build();
+             Long thread_id = get_thread_id(uri, recipient);
+
+             ContentValues values = new ContentValues();
+             values.put("address", recipient);
+             values.put("body", body);
+             values.put("date", System.currentTimeMillis());
+             values.put("type", 1);
+             values.put("thread_id", thread_id);
+             context.getContentResolver().insert(Uri.parse("content://sms/sent"), values);
+         }
+     	
+     	private Long get_thread_id(Uri uri, String recipient) {
+             long threadId = 0;
+             Cursor cursor = context.getContentResolver().query(uri, new String[] { "_id" },
+                     null, null, null);
+             if (cursor != null) {
+                 try {
+                     if (cursor.moveToFirst()) {
+                     	threadId = cursor.getLong(0);
+                     }
+                 } finally {
+                     cursor.close();
+                 }
+             }
+             return threadId;
+         }
       
 }
